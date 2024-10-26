d9b7a296482b14fb91aff75d9e0d8e07184c0407
Rogers Billieri <rogersbjunior@gmail.com>
2024-10-26 19:02:31 -0300

    fix: nullable warnings

--- Backend/src/Backend.Domain/Repositories/IRepository.cs
+++ Backend/src/Backend.Domain/Repositories/IRepository.cs
@@ -12,9 +12,9 @@ public interface IRepository<TEntity> where TEntity : class
     Task UpdateAsync(TEntity entity);
     void Delete(TEntity entity);
     Task DeleteAsync(TEntity entity);
-    Task<int> Count(IEnumerable<Expression<Func<TEntity, bool>>> predicates = null);
+    Task<int> Count(IEnumerable<Expression<Func<TEntity, bool>>>? predicates = null);
     Task<bool> ExistAsync(Expression<Func<TEntity, bool>> predicate);
-    Task<TEntity> GetByIdAsync(int id, IEnumerable<string> entitiesToInclude = null);
-    Task<IEnumerable<TEntity>> GetAllAsync(IEnumerable<Expression<Func<TEntity, bool>>> predicates = null, IEnumerable<string> entitiesToInclude = null);
-    Task<IEnumerable<TEntity>> GetAllAsync(int skip, int limit, IEnumerable<Expression<Func<TEntity, bool>>> predicates = null, IEnumerable<string> entitiesToInclude = null);
+    Task<TEntity> GetByIdAsync(int id, IEnumerable<string>? entitiesToInclude = null);
+    Task<IEnumerable<TEntity>> GetAllAsync(IEnumerable<Expression<Func<TEntity, bool>>>? predicates = null, IEnumerable<string>? entitiesToInclude = null);
+    Task<IEnumerable<TEntity>> GetAllAsync(int skip, int limit, IEnumerable<Expression<Func<TEntity, bool>>>? predicates = null, IEnumerable<string>? entitiesToInclude = null);
 }
