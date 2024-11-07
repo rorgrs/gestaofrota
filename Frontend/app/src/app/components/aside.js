044efa34b6c803cc77099f1ce2c3ed912629796d
Viniciusfortuna <vini.s.fortuna@gmail.com>
2024-11-07 14:13:54 -0300

    correcao

--- Frontend/app/src/app/components/aside.js
+++ Frontend/app/src/app/components/aside.js
@@ -2,10 +2,12 @@ import Link from "next/link"
 
 export default function Aside(){
     return (
-        <Link href="/" className="block text-lg font-bold text-gray-800 hover:text-blue-500">Página Inicial</Link>
-        <Link href="/motorista/readAll" className="block text-lg font-bold text-gray-800 hover:text-blue-500">Motoristas</Link>
-        <Link href="/usuarios/readAll" className="block text-lg font-bold text-gray-800 hover:text-blue-500">Usuarios</Link>
-        <Link href="/viagem/readAll" className="block text-lg font-bold text-gray-800 hover:text-blue-500">Viagens</Link>
-        <Link href="/veiculos/readAll" className="block text-lg font-bold text-gray-800 hover:text-blue-500">Veiculos</Link>
+        <div className="aside">
+            <Link href="/" className="block text-lg font-bold text-gray-800 hover:text-blue-500">Página Inicial</Link>
+            <Link href="/motorista/readAll" className="block text-lg font-bold text-gray-800 hover:text-blue-500">Motoristas</Link>
+            <Link href="/usuarios/readAll" className="block text-lg font-bold text-gray-800 hover:text-blue-500">Usuarios</Link>
+            <Link href="/viagem/readAll" className="block text-lg font-bold text-gray-800 hover:text-blue-500">Viagens</Link>
+            <Link href="/veiculos/readAll" className="block text-lg font-bold text-gray-800 hover:text-blue-500">Veiculos</Link>
+        </div>
     );
 }
